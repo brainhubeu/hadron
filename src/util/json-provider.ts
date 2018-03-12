1ac2fcc6fceb8325b4518fc5b207cbcea82abcac
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-12 11:08:04 +0100

    json-provider refactor

--- src/util/json-provider.ts
+++ src/util/json-provider.ts
@@ -1,16 +1,29 @@
 import * as fs from "fs";
-import { relative } from "path";
+import { extname, relative } from "path";
 import { parseString as xmlToJson } from "xml2js";
 import locate from "./file-locator";
 
 const getExtension = (path: string): string => {
-    return path.split(".")[path.split(".").length - 1].toLowerCase();
+    return extname(path).substring(1);
+};
+
+export const jsLoader = (path: string) => {
+    const supportsExtension: string = "js";
+    return new Promise((resolve, reject) => {
+        if (getExtension(path) !== supportsExtension) {
+            reject(new Error(`${path} doesn't have ${supportsExtension} extension`));
+        }
+
+        const data = require(`./${relative(__dirname, path)}`);
+        data !== null ? resolve(data()) : reject(new Error("File not found"));
+    });
 };
 
 export const jsonLoader = (path: string) => {
+    const supportsExtension: string = "json";
     return new Promise((resolve, reject) => {
-        if (getExtension(path) !== Object.keys(mapper)[1]) {
-            reject(new Error(`${path} doesn't have a ${Object.keys(mapper)[1]} extension`));
+        if (getExtension(path) !== supportsExtension) {
+            reject(new Error(`${path} doesn't have a ${supportsExtension} extension`));
         }
 
         fs.readFile(path, "utf8", (err, data) => {
@@ -19,21 +32,11 @@ export const jsonLoader = (path: string) => {
     });
 };
 
-export const jsLoader = (path: string) => {
-    return new Promise((resolve, reject) => {
-        if (getExtension(path) !== Object.keys(mapper)[0]) {
-            reject(new Error(`${path} doesn't have ${Object.keys(mapper)[0]} extension`));
-        }
-
-        const data = require(`./${relative(__dirname, path)}`);
-        data !== null ? resolve(data()) : reject(new Error("File not found"));
-    });
-};
-
 export const xmlLoader = (path: string) => {
+    const supportsExtension: string = "xml";
     return new Promise((resolve, reject) => {
-        if (getExtension(path) !== Object.keys(mapper)[2]) {
-            reject(new Error(`${path} doesn't have ${Object.keys(mapper)[2]} extension`));
+        if (getExtension(path) !== supportsExtension) {
+            reject(new Error(`${path} doesn't have ${supportsExtension} extension`));
         }
 
         fs.readFile(path, "utf8", (err, data) => {
@@ -58,13 +61,11 @@ const mapper: any = {
 };
 
 const extensionMapper = (paths: string[]): Array<Promise<any>> => {
-    return paths.map((path) => {
-        const ext = path.split(".")[path.split(".").length - 1];
-        return mapper[ext](path);
-    });
+    return paths.map((path) => mapper[getExtension(path)](path));
 };
 
-const jsonProvider = (paths: string[], configName: string, type: string, extensions: string[] = []) => {
+const jsonProvider = (paths: string[], configName: string,
+                      type: string, extensions: string[] = []): Promise<object> => {
     return locate(paths, configName, type, extensions)
     .then((locatedPaths) => Promise.all(extensionMapper(locatedPaths)))
     .then((data) => Object.assign({}, ...data));
