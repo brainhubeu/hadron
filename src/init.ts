19325865cf27875ffe2ecd48ffd86c04ebc3225f
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-15 14:56:03 +0100

    add EventEmitter to container

--- src/init.ts
+++ src/init.ts
@@ -1,2 +1,8 @@
+
 import * as dotenv from 'dotenv';
+import Container from './containers/container';
+import { EventEmitter } from 'events';
+import { Lifetime } from "containers/lifetime";
 dotenv.config(); // we need this before anything that may need envs
+// default container items
+Container.register("emitter", EventEmitter, Lifetime.Singletone);
