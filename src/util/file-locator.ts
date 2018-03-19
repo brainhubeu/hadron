375a020d746bf4686dd622959086e2211e05dc77
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-19 09:56:30 +0100

    Tslint fix

--- src/util/file-locator.ts
+++ src/util/file-locator.ts
@@ -1,5 +1,38 @@
 import glob from './glob-promise';
 
+const addExtension = (paths: string[], extensions: string[]): string[] => {
+  const pathsWithExtension: string[] = [];
+
+  extensions.map(ext => {
+    paths.map(path => pathsWithExtension.push(`${path}.${ext.toLowerCase()}`));
+  });
+
+  return pathsWithExtension;
+};
+
+const filterPaths = (data: string[], configName: string, type: string) =>
+    new Promise((resolve, reject) => {
+      const arr: string[] = [];
+
+      data.forEach(element => {
+        const fileName = (element.split('/')[element.split('/').length - 1]).split('.')[0];
+
+        if (fileName === configName) {
+          arr.push(element);
+        } else if (fileName.includes('_')) {
+          if (fileName.split('_')[0] === configName && fileName.split('_')[1] === type) {
+            arr.push(element);
+          }
+        }
+      });
+
+      resolve(arr);
+    });
+
+
+const flattenDeep = (arr: any[]): any[] =>
+Array.isArray(arr) ? arr.reduce((a, b) => [...flattenDeep(a), ...flattenDeep(b)], []) : [arr];
+
 export const configLocate = (paths: string[], configName: string, type: string,
                              extensions: string[] = []): Promise<any> =>
     new Promise((resolve, reject) => {
@@ -23,39 +56,5 @@ export const locate = (paths: string[], extensions: string[] = []): Promise<any>
 
       Promise.all(paths.map(glob)).then(data => flattenDeep(data.map(el => el.sort()))).then(resolve);
     });
-
-const addExtension = (paths: string[], extensions: string[]): string[] => {
-  const pathsWithExtension: string[] = [];
-
-  extensions.map(ext => {
-    paths.map(path => pathsWithExtension.push(`${path}.${ext.toLowerCase()}`));
-  });
-
-  return pathsWithExtension;
-};
-
-const flattenDeep = (arr: any[]): any[] =>
-    Array.isArray(arr) ? arr.reduce((a, b) => [...flattenDeep(a), ...flattenDeep(b)], []) : [arr];
-
-
-const filterPaths = (data: string[], configName: string, type: string) =>
-    new Promise((resolve, reject) => {
-      const arr: string[] = [];
-
-      data.forEach(element => {
-        const fileName = (element.split('/')[element.split('/').length - 1]).split('.')[0];
-
-        if (fileName === configName) {
-          arr.push(element);
-        } else if (fileName.includes('_')) {
-          if (fileName.split('_')[0] === configName && fileName.split('_')[1] === type) {
-            arr.push(element);
-          }
-        }
-      });
-
-      resolve(arr);
-    });
-
 
 export default locate;
