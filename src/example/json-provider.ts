481efe6a0668a69f55b6e7a25bd1b90a639e80ad
Mikołaj <mikolaj@Mikoajs-MacBook-Pro.local>
2018-03-19 09:51:05 +0100

    Tslint fix

--- src/example/json-provider.ts
+++ src/example/json-provider.ts
@@ -1,5 +1,5 @@
-import { configJsonProvider } from "../util/json-provider";
+import { configJsonProvider } from '../util/json-provider';
 
-configJsonProvider(["src/util/__tests__/mock/app/config/*"], "config", "test", ["json", "js"])
+configJsonProvider(['src/util/__tests__/mock/app/config/*'], 'config', 'test', ['json', 'js'])
 // tslint:disable-next-line:no-console
 .then(data => console.log(data));
--- src/util/file-locator.ts
+++ src/util/file-locator.ts
@@ -1,64 +1,61 @@
-import glob from "./glob-promise";
+import glob from './glob-promise';
 
 export const configLocate = (paths: string[], configName: string, type: string,
-                             extensions: string[] = []): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        paths.map((path) => path.toLowerCase());
+                             extensions: string[] = []): Promise<any> =>
+    new Promise((resolve, reject) => {
+      paths.map(path => path.toLowerCase());
+      if (extensions.length > 0) {
+        paths = addExtension(paths, extensions);
+      }
 
-        if (extensions.length > 0) {
-            paths = addExtension(paths, extensions);
-        }
-
-        Promise.all(paths.map(glob)).then((data) => {
-             filterPaths(flattenDeep(data.map((el) => el.sort())), configName, type).then(resolve);
-        });
+      Promise.all(paths.map(glob)).then(data => {
+        filterPaths(flattenDeep(data.map(el => el.sort())), configName, type).then(resolve);
+      });
     });
-};
 
-export const locate = (paths: string[], extensions: string[] = []): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        paths.map((path) => path.toLowerCase());
+export const locate = (paths: string[], extensions: string[] = []): Promise<any> =>
+    new Promise((resolve, reject) => {
+      paths.map(path => path.toLowerCase());
 
-        if (extensions.length > 0) {
-            paths = addExtension(paths, extensions);
-        }
+      if (extensions.length > 0) {
+        paths = addExtension(paths, extensions);
+      }
 
-        Promise.all(paths.map(glob)).then((data) => flattenDeep(data.map((el) => el.sort()))).then(resolve);
+      Promise.all(paths.map(glob)).then(data => flattenDeep(data.map(el => el.sort()))).then(resolve);
     });
-};
 
 const addExtension = (paths: string[], extensions: string[]): string[] => {
-    const pathsWithExtension: string[] = [];
+  const pathsWithExtension: string[] = [];
 
-    extensions.map((ext) => {
-        paths.map((path) => pathsWithExtension.push(`${path}.${ext.toLowerCase()}`));
+  extensions.map(ext => {
+    paths.map(path => pathsWithExtension.push(`${path}.${ext.toLowerCase()}`));
+  });
+
+  return pathsWithExtension;
+};
+
+const flattenDeep = (arr: any[]): any[] =>
+    Array.isArray(arr) ? arr.reduce((a, b) => [...flattenDeep(a), ...flattenDeep(b)], []) : [arr];
+
+
+const filterPaths = (data: string[], configName: string, type: string) =>
+    new Promise((resolve, reject) => {
+      const arr: string[] = [];
+
+      data.forEach(element => {
+        const fileName = (element.split('/')[element.split('/').length - 1]).split('.')[0];
+
+        if (fileName === configName) {
+          arr.push(element);
+        } else if (fileName.includes('_')) {
+          if (fileName.split('_')[0] === configName && fileName.split('_')[1] === type) {
+            arr.push(element);
+          }
+        }
+      });
+
+      resolve(arr);
     });
 
-    return pathsWithExtension;
-};
-
-const flattenDeep = (arr: any[]): any[] => {
-    return Array.isArray(arr) ? arr.reduce((a, b) => [...flattenDeep(a), ...flattenDeep(b)], []) : [arr];
-};
-
-const filterPaths = (data: string[], configName: string, type: string) => {
-    return new Promise((resolve, reject) => {
-        const arr: string[] = [];
-
-        data.forEach((element) => {
-            const fileName = (element.split("/")[element.split("/").length - 1]).split(".")[0];
-
-            if (fileName === configName) {
-                arr.push(element);
-            } else if (fileName.includes("_")) {
-                if (fileName.split("_")[0] === configName && fileName.split("_")[1] === type) {
-                    arr.push(element);
-                }
-            }
-        });
-
-        resolve(arr);
-    });
-};
 
 export default locate;
