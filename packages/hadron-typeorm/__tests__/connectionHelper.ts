25b3356633b54fef1b23f16c77ae5075c924fe64
Damian <damian@MacBook-Pro-6.local>
2018-03-20 15:37:07 +0100

    added tests

---
+++ packages/hadron-typeorm/__tests__/connectionHelper.ts
@@ -0,0 +1,61 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as typeorm from 'typeorm';
+import { createConnection, createDatabaseConnection } from '../src/connectionHelper';
+
+describe('Connection helper', () => {
+  let items = {};
+  const conn = createDatabaseConnection(
+    'connName', 'MySQL', 'hostAddr',
+    8080, 'userName', 'password',
+    'dbName',
+  );
+  const containerMock = {
+    register: (key: string, value: any) => {
+      items[key] = value;
+    },
+    take: (key: string) => items[key],
+  }
+
+  before(() => {
+    items = {};
+  });
+
+  it('Create database connection object', () => {
+    expect(conn).to.deep.equal({
+      ...conn,
+      name: 'connName',
+      type: 'MySQL',
+      host: 'hostAddr',
+      port: 8080,
+      username: 'userName',
+      password: 'password',
+      database: 'dbName',
+    });
+  });
+
+  describe('Create connection', () => {
+    const stub = sinon.stub(typeorm, 'createConnections');
+    stub.returns(new Promise((resolve, reject) => {
+      resolve([conn]);
+    }))
+
+    const register = sinon.spy(containerMock, 'register');
+
+    it('Registering repositories', () => {
+      return createConnection(containerMock, {
+        connections: [conn],
+      }).then(() => {
+        sinon.assert.calledWith(register, 'repositories');
+      });
+    });
+
+    it('Registering connections', () => {
+      return createConnection(containerMock, {
+        connections: [conn],
+      }).then(() => {
+        sinon.assert.calledWith(register, 'connections', [conn]);
+      });
+    });
+  });
+});
