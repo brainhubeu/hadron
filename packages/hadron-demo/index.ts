cb5490cebe278e395b93414b55cd635bf921ea66
Damian <damian@MacBook-Pro-6.local>
2018-03-21 13:02:32 +0100

    added some use cases

--- packages/hadron-demo/index.ts
+++ packages/hadron-demo/index.ts
@@ -1,12 +1,18 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import { register as expressRegister } from '../hadron-express';
+import expressConfig from './express';
 
-/*
-import BF from 'brainhub-framework';
-BF.configureApp()
-*/
+import hadron, { Container as container } from '../hadron-core';
+
 const port = process.env.PORT || 8080;
 const expressApp = express();
-
 expressApp.use(bodyParser.json());
-expressApp.use(bodyParser.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
+
+container.register('customValue', 'From Brainhub with ❤️');
+
+hadron(expressApp, [
+  import('../hadron-express'),
+], expressConfig);
+
+expressApp.listen(port);
