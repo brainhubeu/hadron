0d93e5c1db402b59051695610b011f2ce17d8a3a
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-21 09:50:14 +0100

    remove container config

--- src/init.ts
+++ src/init.ts
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
 import { Lifetime } from 'containers/lifetime';
 import * as dotenv from 'dotenv';
 import { EventEmitter } from 'events';
 import Container from './containers/container';
 
 dotenv.config(); // we need this before anything that may need envs
-// default container items
-Container.register('emitter', EventEmitter, Lifetime.Singletone);
-=======
-import * as dotenv from "dotenv";
-
-import Container from './containers/container';
-import { EventEmitter } from 'events';
-import { Lifetime } from "containers/lifetime";
-dotenv.config(); // we need this before anything that may need envs
-
-// default container items
-Container.register("emitter", EventEmitter, Lifetime.Singletone);
->>>>>>> adff7b61c7d7629835fd97e030d4c7608b480fe4
