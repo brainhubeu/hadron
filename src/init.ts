a2290e9da7e7004f9c8753a73fae5457d1a8b9e1
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-21 10:34:08 +0100

    remove unused imports

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,3 @@
-import { Lifetime } from 'containers/lifetime';
 import * as dotenv from 'dotenv';
-import { EventEmitter } from 'events';
-import Container from './containers/container';
 
 dotenv.config(); // we need this before anything that may need envs
