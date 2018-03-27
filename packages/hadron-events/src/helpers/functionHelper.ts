46c4e54a87fdc5f142fcf1aef3bc89cac219f56d
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-27 13:11:22 +0200

    helper functions

---
+++ packages/hadron-events/src/helpers/functionHelper.ts
@@ -0,0 +1,18 @@
+const getArgs = (f: any): string[] => {
+  const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+  const ARGUMENT_NAMES = /([^\s,]+)/g;
+  const funcContent = f.toString().replace(STRIP_COMMENTS, '');
+  return funcContent
+        .slice(
+            funcContent.indexOf('(') + 1,
+            funcContent.indexOf(')'),
+        )
+        .match(ARGUMENT_NAMES) || [];
+};
+
+function hasFunctionArgument(func: any, argumentName: any) {
+  return getArgs(func).indexOf(argumentName) >= 0;
+}
+
+export { getArgs };
+export { hasFunctionArgument };
