63cdd9a9b7559ff809b6e68ee9b675d71b28adbd
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-28 12:10:28 +0200

    pass listeners to registerEvents

--- packages/hadron-events/index.ts
+++ packages/hadron-events/index.ts
@@ -6,10 +6,12 @@ import { Lifetime } from '../hadron-core';
 export * from './src/types';
 export default eventsManagerProvider;
 
+
 export const register = (container: any, config: any) => {
   if (container.take(constants.EVENT_EMITTER) === null) {
     container.register(constants.EVENT_EMITTER, EventEmitter, Lifetime.Singletone);
   }
   const eventsManager = eventsManagerProvider(container.take(constants.EVENT_EMITTER), config.events);
+  eventsManager.registerEvents(config.events.listeners);
   container.register(constants.EVENTS_MANAGER, eventsManager);
 }
