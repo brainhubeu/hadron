55db6e2245fb58b9dd63529600b1d08c8aa48be9
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-28 12:04:03 +0200

    remove unused import

--- packages/hadron-demo/event-emitter/config.ts
+++ packages/hadron-demo/event-emitter/config.ts
@@ -1,36 +1,14 @@
-import { ICallbackEvent } from '../../hadron-events/src/types';
 
 const emitterConfig: any = {};
 emitterConfig.listeners = [
   {
-    name: 'LISTENER-1',
-    event: 'someEvent', // event to listen to
-    handler: (...params: any[]) => (event: ICallbackEvent) => {
-      const original = event.callback;
-      // tslint:disable:no-console
-      console.log('my name is LISTENER-1 and I listen to event someEvent');
-      // TODO
-      event.callback = () => {
-        console.log('callback one');
-        return original();
-      };
-    },
-  },
-  {
     name: 'LISTENER-2',
     event: 'createRoutesEvent', // event to listen to
-    handler: (...params: any[]) => (event: ICallbackEvent) => {
-        // tslint:disable:no-console
-      console.log('my name is LISTENER-2 and I listen to event createRoutesEvent');
-    },
-  },
-  {
-    name: 'LISTENER-3',
-    event: 'someEvent', // event to listen to
-    handler: (...params: any[]) => (event: ICallbackEvent) => {
-      // const original = event.callback;
-      // tslint:disable:no-console
-      console.log('my name is LISTENER-3 and I listen to event someEvent');
+    handler: (callback: any, ...args: any[]) => {
+      console.log(Date.now());
+      const result = callback(...args);
+      console.log(Date.now());
+      return 'to';
     },
   },
 ];
