4abdf6c6ff19f2f264af4f3a59e32c842fa5bdc5
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-30 12:12:37 +0200

    remove constants, add hardcoded names

--- packages/hadron-events/index.ts
+++ packages/hadron-events/index.ts
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import constants from './src/constants/constants';
+=======
+>>>>>>> remove constants, add hardcoded names
 import eventsManagerProvider from './src/eventsMaganerProvider';
 import { EventEmitter } from 'events';
 import { Lifetime } from '../hadron-core';
@@ -7,6 +10,7 @@ export * from './src/types';
 export default eventsManagerProvider;
 
 export const register = (container: any, config: any) => {
+<<<<<<< HEAD
   if (container.take(constants.EVENT_EMITTER) === null) {
     container.register(
       constants.EVENT_EMITTER,
@@ -21,3 +25,12 @@ export const register = (container: any, config: any) => {
   eventsManager.registerEvents(config.events.listeners);
   container.register(constants.EVENTS_MANAGER, eventsManager);
 };
+=======
+  if (container.take('event-emitter') === null) {
+    container.register('event-emitter', EventEmitter, Lifetime.Singletone);
+  }
+  const eventsManager = eventsManagerProvider(container.take('event-emitter'), config.events);
+  eventsManager.registerEvents(config.events.listeners);
+  container.register('events-manager', eventsManager);
+}
+>>>>>>> remove constants, add hardcoded names
