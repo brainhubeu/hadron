ac41d9eee66c21e17805d7045c3ec4937733f51f
Maciej Cąderek <maciej.caderek@gmail.com>
2018-03-30 19:26:30 +0200

    Lint fix

--- packages/hadron-logger/src/logger.ts
+++ packages/hadron-logger/src/logger.ts
@@ -2,15 +2,14 @@ import * as bunyan from 'bunyan';
 import { toCamelCase } from '../../hadron-utils/';
 
 const register = (container: any, config: any) => {
-  let {
-    logger: loggers,
-  } = config;
+  let { logger: loggers } = config;
 
   loggers = loggers instanceof Array ? loggers : [loggers];
 
   loggers.forEach((logger: any) => {
     const log = bunyan.createLogger(logger);
 
+    /* tslint:disable-next-line:strict-boolean-expressions */
     let itemName = logger.name || '';
     if (!itemName.match(/(.*)logger$/gi)) itemName += ' logger';
     itemName = toCamelCase(itemName);
@@ -19,6 +18,4 @@ const register = (container: any, config: any) => {
 };
 
 export default register;
-export {
-  register,
-};
+export { register };
