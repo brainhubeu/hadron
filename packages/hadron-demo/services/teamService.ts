f90fe0343fbda93765c75979973c3aa1dac65190
Mikolaj <mikolaj@brainhub.pl>
2018-03-30 13:39:54 +0200

    added response to demo teamService

--- packages/hadron-demo/services/teamService.ts
+++ packages/hadron-demo/services/teamService.ts
@@ -7,13 +7,14 @@ class TeamDto {
   constructor(public id: number, public name: string, public amount: number) { }
 }
 
-const getAllTeams = async(teamRepository: Repository<Team>) =>
-  await teamRepository.find({relations : ['users']})
-    .then(teams => teams.map(team => new TeamDto(team.id, team.name, team.users.length)));
+const getAllTeams = async(res: any, teamRepository: Repository<Team>) =>
+  res.successGet(await teamRepository.find({relations : ['users']})
+    .then(teams => teams.map(team => new TeamDto(team.id, team.name, team.users.length))));
 
-const getTeamById = async(teamRepository: Repository<Team>, id: number) => await teamRepository.findOneById(id);
+const getTeamById = async(res: any, teamRepository: Repository<Team>, id: number) =>
+  res.successGet(await teamRepository.findOneById(id));
 
-const updateTeam = async(teamRepository: Repository<Team>, body: { id: number, teamName: string }) => {
+const updateTeam = async(res: any, teamRepository: Repository<Team>, body: { id: number, teamName: string }) => {
   try {
     await validate('updateTeam', body);
     return await teamRepository.findOneById(body.id)
@@ -21,27 +22,27 @@ const updateTeam = async(teamRepository: Repository<Team>, body: { id: number, t
             team.name = body.teamName;
             return teamRepository.save(team);
           })
-          .then(() => `team id: ${body.id} has new name ${body.teamName}`);
+          .then(() => res.successUpdate(`team id: ${body.id} has new name ${body.teamName}`));
   } catch (error) {
-    throw error;
+    res.entityValidationError(error);
   }
 };
 
-const insertTeam = async(teamRepository: Repository<Team>, body: { teamName: string }) => {
+const insertTeam = async(res: any, teamRepository: Repository<Team>, body: { teamName: string }) => {
   try {
     await validate('insertTeam', body);
     return await teamRepository.insert({name: body.teamName})
       .then(() => teamRepository.count())
-      .then(amount => `total amount of teams: ${amount}`);
-  } catch (err) {
-    throw err;
+      .then(amount => res.successUpdate(`total amount of teams: ${amount}`));
+  } catch (error) {
+    res.entityValidationError(error);
   }
 };
 
-const deleteTeam = async(teamRepository: Repository<Team>, userRepository: Repository<User>, id: number) => {
+const deleteTeam = async(res: any, teamRepository: Repository<Team>, userRepository: Repository<User>, id: number) => {
   await teamRepository.findOneById(id, { relations: ['users']})
     .then(team => userRepository.removeByIds(team.users.map(user => user.id)))
-    .then(() => teamRepository.removeById(id));
+    .then(() => res.successUpdate(teamRepository.removeById(id)));
 };
 
 export { getAllTeams, getTeamById, updateTeam, insertTeam, deleteTeam };
