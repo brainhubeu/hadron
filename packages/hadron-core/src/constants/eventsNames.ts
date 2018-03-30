92155812e5b18bc99ff896b34fbe0f54dc45689e
Wojtek <wojtek@Wojteks-MacBook-Pro.local>
2018-03-30 13:42:36 +0200

    add new event

--- packages/hadron-core/src/constants/eventsNames.ts
+++ packages/hadron-core/src/constants/eventsNames.ts
@@ -1,3 +1,4 @@
 export enum eventsNames {
     HANDLE_INITIALIZE_APPLICATION_EVENT = 'handleInitializeApplicationEvent',
+    HANDLE_TERMINATE_APPLICATION_EVENT = 'handleTerminateApplicationEvent',
 }
