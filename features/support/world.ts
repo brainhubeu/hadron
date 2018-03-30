0346e5f5431572234bdd5507532759fccf163930
Maciej Cąderek <maciej.caderek@gmail.com>
2018-03-30 18:50:49 +0200

    added lint formatting

--- features/support/world.ts
+++ features/support/world.ts
@@ -1,6 +1,6 @@
 import { defineSupportCode } from 'cucumber';
 
-/* tslint:disable:only-arrow-functions ter-prefer-arrow-callback  */
+/* tslint:disable:only-arrow-functions ter-prefer-arrow-callback */
 
 defineSupportCode(function({ setWorldConstructor }) {
   const customWorld = function() {
