7eea5bd2f5c0b309c856bee600cbed746f2d388b
Damian <damian@MacBook-Pro-6.local>
2018-04-03 10:47:48 +0200

    refactored tests

--- packages/hadron-logger/src/__tests__/logger.ts
+++ packages/hadron-logger/src/__tests__/logger.ts
@@ -1,27 +1,24 @@
 import { assert } from 'chai';
+import * as sinon from 'sinon';
 import { register } from '../logger';
-
-let items: any = {};
 
 describe('logger', () => {
   const containerMock = {
-    register: (key: string, value: any) => {
-      items[key] = value
-    },
-    take: (key: string) => items[key],
-  }
+    register: sinon.stub(),
+    take: () => null,
+  };
 
   beforeEach(() => {
-    items = {};
+    containerMock.register.reset();
   });
 
-  it('should register logger', () => {
+  it('should register logger under firstLogger', () => {
     register(containerMock, {
       logger: {
         name: 'first logger',
       },
     });
-    assert(containerMock.take('firstLogger'));
+    assert(containerMock.register.calledWith('firstLogger'));
   });
   it('should register "hello world" under helloWorldLogger', () => {
     register(containerMock, {
@@ -29,7 +26,7 @@ describe('logger', () => {
         name: 'hello world',
       },
     });
-    assert(containerMock.take('helloWorldLogger'));
+    assert(containerMock.register.calledWith('helloWorldLogger'));
   });
   it('should register "hello world logger" under helloWorldLogger', () => {
     register(containerMock, {
@@ -37,7 +34,7 @@ describe('logger', () => {
         name: 'hello world logger',
       },
     });
-    assert(containerMock.take('helloWorldLogger'));
+    assert(containerMock.register.calledWith('helloWorldLogger'));
   });
   it('should register multiple loggers', () => {
     register(containerMock, {
@@ -51,13 +48,13 @@ describe('logger', () => {
       ],
     });
 
-    assert(containerMock.take('firstLogger'));
-    assert(containerMock.take('secondLogger'));
-  })
-  it('should not register without name', () => {
+    assert(containerMock.register.calledWith('firstLogger'));
+    assert(containerMock.register.calledWith('secondLogger'));
+  });
+  it('should not register logger without name', () => {
     register(containerMock, {
       logger: {},
     });
-    assert(!containerMock.take('logger'));
-  })
-})
+    assert(!containerMock.register.calledWith('logger'));
+  });
+});
