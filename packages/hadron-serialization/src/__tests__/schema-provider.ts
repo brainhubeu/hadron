ab75c9f3dfb594b0491cc01077d03449601cd978
Szymon Morawski <szymon.morawski@brainhub.pl>
2018-04-03 09:29:58 +0200

    fixed tests + removed unused files

--- packages/hadron-serialization/src/__tests__/schema-provider.ts
+++ packages/hadron-serialization/src/__tests__/schema-provider.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { ISerializationSchema, ISerializerConfig } from '../types';
-import * as jsonProvider from '@hadron/json-provider';
+import * as jsonProvider from '@brainhubeu/hadron-json-provider';
 import schemaProvider from '../schema-provider';
 
 describe('schemaProvider', () => {
