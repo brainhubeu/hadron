6560c34d4e64330c912050de0e3eafb405e3d4a6
Damian <damian@MacBook-Pro-6.local>
2018-04-05 09:35:06 +0200

    repositoryName function fix

--- packages/hadron-typeorm/src/connectionHelper.ts
+++ packages/hadron-typeorm/src/connectionHelper.ts
@@ -2,8 +2,7 @@ import { createConnection, Connection } from 'typeorm';
 import { CONNECTION } from './constants';
 import { IContainer } from '../../hadron-core/src/container/types';
 
-const repositoryName = (name: string) =>
-  name.toLowerCase ? `${name.toLowerCase()}Repository` : name;
+const repositoryName = (name: string) => `${name.toLowerCase()}Repository`;
 
 const registerRepositories = (
   container: IContainer,
