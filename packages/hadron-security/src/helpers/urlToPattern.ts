118f0fadfd75467d2c00ebb949f36b9aac31e363
Mikolaj <mikolaj@brainhub.pl>
2018-04-10 23:17:56 +0200

    refactor + urlGlob method

--- packages/hadron-security/src/helpers/urlToPattern.ts
+++ packages/hadron-security/src/helpers/urlToPattern.ts
@@ -8,8 +8,13 @@ const replaceAll = (
 
 const convertToPattern = (url: string): string => {
   let regexp = url[0] === '/' ? url.substring(1) : url;
-  regexp = `^/?${regexp}`;
-  regexp = `${regexp.replace('/*', '($|/$|/[^/]*$)')}`;
-  regexp = `${replaceAll(regexp, '/', '\\/')}`;
-  return regexp;
+  regexp = `^/?${regexp.replace('/*', '($|/$|/[^/]*$)')}`;
+  return `${replaceAll(regexp, '/', '\\/')}$`;
 };
+
+const urlGlob = (pattern: string, input: string): boolean => {
+  const re = new RegExp(convertToPattern(pattern));
+  return re.test(input);
+};
+
+export default urlGlob;
