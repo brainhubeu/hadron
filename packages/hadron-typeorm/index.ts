8d29c661867d800092e6d697117e5782150e3432
Damian <damian@MacBook-Pro-6.local>
2018-04-18 16:08:25 +0200

    hadron-typeorm refactor

--- packages/hadron-typeorm/index.ts
+++ packages/hadron-typeorm/index.ts
@@ -1,7 +1,8 @@
 import { connect } from './src/connectionHelper';
 import * as constants from './src/constants';
+import { IHadronTypeormConfig } from './src/types';
 
 export const register = (container: any, config: any): Promise<void> =>
   connect(container, config);
 
-export { connect, constants };
+export { connect, constants, IHadronTypeormConfig };
--- packages/hadron-typeorm/src/connectionHelper.ts
+++ packages/hadron-typeorm/src/connectionHelper.ts
@@ -1,27 +1,45 @@
-import { createConnection, Connection } from 'typeorm';
+import {
+  createConnection,
+  Connection,
+  EntityOptions,
+} from 'typeorm';
 import { CONNECTION } from './constants';
 import { IContainer } from '@brainhubeu/hadron-core';
+import { IHadronTypeormConfig } from './types';
 
 const repositoryName = (name: string) => `${name.toLowerCase()}Repository`;
 
 const registerRepositories = (
   container: IContainer,
   connection: Connection,
-  entities: any[],
+  entities: Array<string | EntityOptions>,
 ) => {
-  entities.forEach((entity: any) => {
-    const name = (entity && entity.name) || entity;
+  entities.forEach((entity: string | EntityOptions) => {
+    let name: string = '';
+
+    if (typeof entity === 'object') {
+      name = entity.name;
+    } else if (typeof entity === 'string') {
+      name = entity;
+    }
+
     container.register(repositoryName(name), connection.getRepository(name));
   });
 };
 
-const registerConnection = (container: IContainer, connection: Connection) => {
+const registerConnection = (
+  container: IContainer,
+  connection: Connection,
+): Connection => {
   container.register(CONNECTION, connection);
   return connection;
 };
 
-const connect = (container: IContainer, config: any): Promise<any> => {
-  const { connection = {} } = config;
+const connect = (
+  container: IContainer,
+  config: IHadronTypeormConfig,
+): Promise<any> => {
+  const { connection } = config;
 
   return createConnection(connection)
     .then((connection) => registerConnection(container, connection))
