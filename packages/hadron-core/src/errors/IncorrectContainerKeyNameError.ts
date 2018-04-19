f1a247250f72ed6e1fd68919f4ae7e2cad24e9d8
Maciej Cąderek <maciej@brainhub.pl>
2018-04-19 11:46:49 +0200

    Update IncorrectContainerKeyNameError.ts

--- packages/hadron-core/src/errors/IncorrectContainerKeyNameError.ts
+++ packages/hadron-core/src/errors/IncorrectContainerKeyNameError.ts
@@ -3,7 +3,7 @@ import HadronErrorHandler from '@brainhubeu/hadron-error-handler';
 export default class IncorrectContainerKeyNameError extends HadronErrorHandler {
   constructor(key: string, err: Error = new Error()) {
     super();
-    this.message = `The key name '${key}' is incorrect to register in container.`;
+    this.message = `The key name '${key}' is incorrect to register in container, should be valid variable name.`;
     this.name = 'IncorrectContainerKeyNameError';
     this.stack = null;
     this.error = err;
