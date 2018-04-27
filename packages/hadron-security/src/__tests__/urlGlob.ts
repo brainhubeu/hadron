2f1e23c9922ddd549a4114b8c6b4f61917b4087a
Mikolaj <mikolaj@brainhub.pl>
2018-04-27 17:24:30 +0200

    added better support for route validation

--- packages/hadron-security/src/__tests__/urlGlob.ts
+++ packages/hadron-security/src/__tests__/urlGlob.ts
@@ -2,19 +2,52 @@ import { expect } from 'chai';
 import urlGlob from '../helpers/urlGlob';
 
 describe('Glob URL pattern', () => {
-  it('should return true if url is valid', () => {
+  it('should return true if URL: /api/admin/qwe is valid for PATTERN: /api/admin/*', () => {
     expect(urlGlob('/api/admin/*', '/api/admin/qwe')).to.be.equal(true);
   });
 
-  it('should return true if strict url is valid', () => {
-    expect(urlGlob('/api/user/1', '/api/user/1')).to.be.equal(true);
+  it('should return false if URL: /api/adm/qwe is invalid for PATTERN: /api/admin/*', () => {
+    expect(urlGlob('/api/admin/*', '/api/adm/qwe')).to.be.equal(false);
   });
 
-  it('should return false if url is invalid', () => {
-    expect(urlGlob('/api/adm/*', '/api/admin')).to.be.equal(false);
+  it('should return false if URL: /api/admin/qwe/1 is invalid for PATTERN: /api/admin/*', () => {
+    expect(urlGlob('/api/admin/*', '/api/adm/qwe')).to.be.equal(false);
   });
 
-  it('should return false if strict url is invalid', () => {
-    expect(urlGlob('/api/adm', '/api/admin')).to.be.equal(false);
+  it('should return true if URL: /api/admin/tasks/1 is valid for PATTERN: /api/admin/**', () => {
+    expect(urlGlob('/api/admin/**', '/api/admin/tasks/1')).to.be.equal(true);
+  });
+
+  it('should return true if URL: /api/something/admin/more/user/in/manager/string for PATTERN: /api/**/admin/**/manager/**', () => {
+    expect(
+      urlGlob(
+        '/api/**/admin/**/user/**/manager/**',
+        '/api/something/admin/more/user/in/manager/string',
+      ),
+    ).to.be.equal(true);
+  });
+
+  it('should return false if URL: /api/something/admin/more/user/in/mage/string for PATTERN: /api/**/admin/**/manager/**', () => {
+    expect(
+      urlGlob(
+        '/api/**/admin/**/user/**/manager/**',
+        '/api/something/admin/more/user/in/mage/string',
+      ),
+    ).to.be.equal(false);
+  });
+
+  it('should return false if URL: /api/something/admin/more is invalid for PATTERNL /api/**/user/**', () => {
+    expect(urlGlob('/api/**/admin/**', '/api/something/user/more')).to.be.equal(
+      false,
+    );
+  });
+
+  it('should return true if URL: /api/admin is valid for strict PATTERN /api/admin', () => {
+    expect(urlGlob('/api/admin', '/api/admin')).to.be.equal(true);
+  });
+
+  it('should return false if URL: /api/adm or /api.admin/1 is invalid for strict PATTERN /api/admin', () => {
+    expect(urlGlob('/api/admin', '/api/adm')).to.be.equal(false);
+    expect(urlGlob('/api/admin', '/api/admin/1')).to.be.equal(false);
   });
 });
--- packages/hadron-security/src/helpers/urlGlob.ts
+++ packages/hadron-security/src/helpers/urlGlob.ts
@@ -1,6 +1,25 @@
+const countStars = (input: string) => {
+  try {
+    return input.match(/\*\*/g).length;
+  } catch (error) {
+    return 0;
+  }
+};
+
 export const convertToPattern = (url: string): string => {
   const regexp = url[0] === '/' ? url.substring(1) : url;
-  return `^/?${regexp.replace(/\/\*/g, '($|/$|/[^/]*$)')}$`;
+  if (url.endsWith('/*') && countStars(url) === 0) {
+    return `^/?${regexp.replace(/\/\*/g, '($|/$|/[^/]*$)')}$`;
+  }
+  if (url.endsWith('/**') && countStars(url) === 1) {
+    return `^/?${regexp.replace(/\/\*\*/g, '($|/.*$)')}$`;
+  }
+
+  if (countStars(url) === 0) {
+    return `^/?${url}$`;
+  }
+
+  return convertToPattern(regexp.replace('**', '[^/]*'));
 };
 
 const urlGlob = (pattern: string, input: string): boolean => {
