d2e3ad571f56a423a86a843c84a42c03c9de0be6
Wojciech Słomczyński <wojciech@brainhub.pl>
2018-05-10 16:18:05 +0200

    add keys() method to proxy

--- packages/hadron-express/src/createContainerProxy.ts
+++ packages/hadron-express/src/createContainerProxy.ts
@@ -2,7 +2,11 @@ import { IContainer } from './types';
 
 const createContainerProxy = (container: IContainer) => {
   return new Proxy(
-    {},
+    {
+      keys() {
+        return container.keys();
+      },
+    },
     {
       get(target, name) {
         return container.take(name as string);
